0adf808a6fe97e71d4e6420a14a088b38f81fc12
Keen <Treven.Keen@outlook.com>
2026-09-09 12:37:02 -0500

    Added Random Math Facts

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
 
+    cout << endl;
+    cout << "************************************************************" << endl;
     cout << R"(
      ____  _ _ _         ____  _                 _
     / ___|(_) | |_   _  / ___|(_)_ __ ___  _ __ | | ___
@@ -13,11 +15,19 @@ int main() {
     |  \/  | __ _| |_| |__   |_   _|   _| |_ ___  _ __
     | |\/| |/ _` | __| '_ \    | || | | | __/ _ \| '__|
     | |  | | (_| | |_| | | |   | || |_| | || (_) | |
-    |_|  |_|\__,_|\__|_| |_|   |_| \__,_|\__\___/|_|    )" <<endl;
-
-    cout << "*****************************************" << endl;
-    cout << "Welcome to the Silly Simply Math Tutor V1" << endl;
-    cout << "*****************************************" << endl;
+    |_|  |_|\__,_|\__|_| |_|   |_| \__,_|\__\___/|_|    )"  << endl;
+    cout << endl;
+    cout << "************************************************************" << endl;
+    cout << "         Welcome to the Silly Simply Math Tutor V1" << endl;
+    cout << "************************************************************" << endl;
+    cout << endl;
+    cout << "Here are some fun facts about math:" << endl;
+    cout << "     - Greek artists used math to properly size their sculptures." << endl;
+    cout << "     - Computers hate dividing by zero and it will often kill the program." << endl;
+    cout << "     - The current number system used in english is base 10, hexidecimal uses base 16." << endl;
+    cout << "     - Most complex math equations have a formula that is easily remembered to get an answer." << endl;
+    cout << endl;
+    cout << "************************************************************" << endl << endl;
 
     return 0;
 }
